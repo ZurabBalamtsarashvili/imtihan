449c5ecd915978d43e9f0317c5ab847dadbd06e6
Ali KARABAY <alikarabay77@gmail.com>
2023-01-06 14:52:53 +0300

    Redux configuration and edit done

--- frontend/src/components/InputSelect.js
+++ frontend/src/components/InputSelect.js
@@ -1,12 +1,19 @@
-export default function InputSelect({ disabled = false, className, defaultOption, children, ...props }) {
-    return(
+import React from 'react';
+
+const InputSelect = React.forwardRef(
+    (
+        { disabled = false, className, defaultOption, children, ...props },
+        ref,
+    ) => (
         <select
+            ref={ref}
             disabled={disabled}
             className={`${className} w-full p-2.5 text-sm rounded-lg border border-brand focus:border-blue-100 dark:bg-gray-800 dark:text-white`}
-            {...props}
-        >
+            {...props}>
             <option>{defaultOption}</option>
             {children}
         </select>
-    )
-}
+    ),
+);
+
+export default InputSelect;
