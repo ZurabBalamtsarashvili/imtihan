f0c37eb6548d787126549042a44f2e280baa4708
Ali KARABAY <alikarabay77@gmail.com>
2023-03-13 00:13:22 +0300

    https module added.

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,4 +1,9 @@
-import Axios from 'axios'
+import Axios from 'axios';
+import https from 'https';
+
+const agent = new https.Agent({
+    rejectUnauthorized: false,
+});
 
 const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
@@ -6,6 +11,7 @@ const axios = Axios.create({
         'X-Requested-With': 'XMLHttpRequest',
     },
     withCredentials: true,
-})
+    httpAgent: agent,
+});
 
-export default axios
+export default axios;
